e51efd1938c06d7cfcecad3ab7df869a17f7ace3
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-09-14 08:49:05 -0400

    partial updated for clarity

--- src/localqtl/pgen.py
+++ src/localqtl/pgen.py
@@ -1,11 +1,12 @@
 # Functions for reading dosages from PLINK pgen files based on the Pgenlib Python API:
 # https://github.com/chrchang/plink-ng/blob/master/2.0/Python/python_api.txt
 
+import os
+import torch
+import bisect
 import numpy as np
 import pandas as pd
 import pgenlib as pg
-import os
-import bisect
 
 
 def read_pvar(pvar_path):
@@ -23,288 +24,16 @@ def read_psam(psam_path):
     return psam_df
 
 
-def hardcall_phase_present(pgen_path):
-    """Returns True iff phased hardcalls may be present"""
-    with pg.PgenReader(pgen_path.encode()) as r:
-        return r.hardcall_phase_present()
-
-
-def get_reader(pgen_path, sample_subset=None):
-    """"""
-    if sample_subset is not None:
-        sample_subset = np.array(sample_subset, dtype=np.uint32)
-    reader = pg.PgenReader(pgen_path.encode(), sample_subset=sample_subset)
-    if sample_subset is None:
-        num_samples = reader.get_raw_sample_ct()
-    else:
-        num_samples = len(sample_subset)
-    return reader, num_samples
-
-
-def read(pgen_path, variant_idx, sample_subset=None, dtype=np.int8):
-    """
-    Get genotypes for a variant.
-
-    Parameters
-    ----------
-    pgen_path : str
-        Path of PLINK 2 pgen file
-    variant_idx : int
-        Variant index
-    sample_subset : array_like
-        List of sample indexes to select. Must be sorted.
-    dtype : np.int{8,32,64}
-        Data type of the returned array.
-
-    Returns
-    -------
-    dosages : ndarray
-        Genotypes (as {0, 1, 2, -9}) for the selected variant and samples.
-    """
-    reader, num_samples = get_reader(pgen_path, sample_subset=sample_subset)
-    genotypes = np.zeros(num_samples, dtype=dtype)
-    with reader as r:
-        r.read(np.array(variant_idx, dtype=np.uint32), genotypes)
-    return genotypes
-
-
-def read_dosages(pgen_path, variant_idx, sample_subset=None, dtype=np.float32):
-    """
-    Get dosages for a variant.
-
-    Parameters
-    ----------
-    pgen_path : str
-        Path of PLINK 2 pgen file
-    variant_idx : int
-        Variant index
-    sample_subset : array_like
-        List of sample indexes to select. Must be sorted.
-    dtype : np.float{32,64}
-        Data type of the returned array.
-
-    Returns
-    -------
-    dosages : ndarray
-        Genotype dosages for the selected variant and samples.
-    """
-    reader, num_samples = get_reader(pgen_path, sample_subset=sample_subset)
-    dosages = np.zeros(num_samples, dtype=dtype)
-    with reader as r:
-        r.read_dosages(np.array(variant_idx, dtype=np.uint32), dosages)
-    return dosages
-
-
-def read_alleles(pgen_path, variant_idx, sample_subset=None):
-    """
-    Get alleles for a variant.
-
-    Parameters
-    ----------
-    pgen_path : str
-        Path of PLINK 2 pgen file
-    variant_idx : int
-        Variant index
-    sample_subset : array_like
-        List of sample indexes to select. Must be sorted.
-
-    Returns
-    -------
-    alleles: ndarray (2 * sample_ct)
-        Alleles for the selected variant and samples.
-        Elements 2n and 2n+1 correspond to sample n.
-        Both elements are -9 for missing genotypes.
-        If the genotype is unphased, the lower index appears first.
-    """
-    reader, num_samples = get_reader(pgen_path, sample_subset=sample_subset)
-    alleles = np.zeros(2*num_samples, dtype=np.int32)
-    with reader as r:
-        r.read_alleles(np.array(variant_idx, dtype=np.uint32), alleles)
-    return alleles
-
-
-def read_list(pgen_path, variant_idxs, sample_subset=None, dtype=np.int8):
-    """
-    Get genotypes for a list of variants.
-
-    Parameters
-    ----------
-    pgen_path : str
-        Path of PLINK 2 pgen file
-    variant_idxs : array_like
-        List of variant indexes
-    sample_subset : array_like
-        List of sample indexes to select. Must be sorted.
-    dtype : np.int{8,32,64}
-        Data type of the returned array.
-
-    Returns
-    -------
-    dosages : ndarray
-        Genotypes for the selected variants and samples.
-    """
-    reader, num_samples = get_reader(pgen_path, sample_subset=sample_subset)
-    num_variants = len(variant_idxs)
-    genotypes = np.zeros([num_variants, num_samples], dtype=dtype)
-    with reader as r:
-        r.read_list(np.array(variant_idxs, dtype=np.uint32), genotypes)
-    return genotypes
-
-
-def read_dosages_list(pgen_path, variant_idxs, sample_subset=None, dtype=np.float32):
-    """
-    Get dosages for a list of variants.
-
-    Parameters
-    ----------
-    pgen_path : str
-        Path of PLINK 2 pgen file
-    variant_idxs : array_like
-        List of variant indexes
-    sample_subset : array_like
-        List of sample indexes to select. Must be sorted.
-    dtype : np.float{32,64}
-        Data type of the returned array.
-
-    Returns
-    -------
-    dosages : ndarray
-        Genotype dosages for the selected variants and samples.
-    """
-    reader, num_samples = get_reader(pgen_path, sample_subset=sample_subset)
-    num_variants = len(variant_idxs)
-    dosages = np.zeros([num_variants, num_samples], dtype=dtype)
-    with reader as r:
-        r.read_dosages_list(np.array(variant_idxs, dtype=np.uint32), dosages)
-    return dosages
-
-
-def read_alleles_list(pgen_path, variant_idxs, sample_subset=None):
-    """
-    Get alleles for a list of variants.
-
-    Parameters
-    ----------
-    pgen_path : str
-        Path of PLINK 2 pgen file
-    variant_idxs : array_like
-        List of variant indexes
-    sample_subset : array_like
-        List of sample indexes to select. Must be sorted.
-
-    Returns
-    -------
-    alleles : ndarray
-        Alleles for the selected variants and samples.
-    """
-    reader, num_samples = get_reader(pgen_path, sample_subset=sample_subset)
-    num_variants = len(variant_idxs)
-    alleles = np.zeros([num_variants, 2*num_samples], dtype=np.int32)
-    with reader as r:
-        r.read_alleles_list(np.array(variant_idxs, dtype=np.uint32), alleles)
-    return alleles
-
-
-def read_range(pgen_path, start_idx, end_idx, sample_subset=None, dtype=np.int8):
-    """
-    Get genotypes for a range of variants.
-
-    Parameters
-    ----------
-    pgen_path : str
-        Path of PLINK 2 pgen file
-    start_idx : int
-        Start index of the range to query.
-    end_idx : int
-        End index of the range to query (inclusive).
-    sample_subset : array_like
-        List of sample indexes to select. Must be sorted.
-    dtype : np.int{8,32,64}
-        Data type of the returned array.
-
-    Returns
-    -------
-    dosages : ndarray
-        Genotypes for the selected variants and samples.
-    """
-    reader, num_samples = get_reader(pgen_path, sample_subset=sample_subset)
-    num_variants = end_idx - start_idx + 1
-    genotypes = np.zeros([num_variants, num_samples], dtype=dtype)
-    with reader as r:
-        r.read_range(start_idx, end_idx+1, genotypes)
-    return genotypes
-
-
-def read_dosages_range(pgen_path, start_idx, end_idx, sample_subset=None, dtype=np.float32):
-    """
-    Get dosages for a range of variants.
-
-    Parameters
-    ----------
-    pgen_path : str
-        Path of PLINK 2 pgen file
-    start_idx : int
-        Start index of the range to query.
-    end_idx : int
-        End index of the range to query (inclusive).
-    sample_subset : array_like
-        List of sample indexes to select. Must be sorted.
-    dtype : np.float{32,64}
-        Data type of the returned array.
-
-    Returns
-    -------
-    dosages : ndarray
-        Genotype dosages for the selected variants and samples.
-    """
-    reader, num_samples = get_reader(pgen_path, sample_subset=sample_subset)
-    num_variants = end_idx - start_idx + 1
-    dosages = np.zeros([num_variants, num_samples], dtype=dtype)
-    with reader as r:
-        r.read_dosages_range(start_idx, end_idx+1, dosages)
-    return dosages
-
-
-def read_alleles_range(pgen_path, start_idx, end_idx, sample_subset=None):
-    """
-    Get alleles for a range of variants.
-
-    Parameters
-    ----------
-    pgen_path : str
-        Path of PLINK 2 pgen file
-    start_idx : int
-        Start index of the range to query.
-    end_idx : int
-        End index of the range to query (inclusive).
-    sample_subset : array_like
-        List of sample indexes to select. Must be sorted.
-
-    Returns
-    -------
-    alleles : ndarray
-        Alleles for the selected variants and samples.
-    """
-    reader, num_samples = get_reader(pgen_path, sample_subset=sample_subset)
-    num_variants = end_idx - start_idx + 1
-    alleles = np.zeros([num_variants, 2*num_samples], dtype=np.int32)
-    with reader as r:
-        r.read_alleles_range(start_idx, end_idx+1, alleles)
-    return alleles
-
-
-def _impute_mean(genotypes):
-    """Impute missing genotypes to mean"""
-    m = genotypes == -9
-    if genotypes.ndim == 1 and any(m):
-        genotypes[m] = genotypes[~m].mean()
+def _impute_mean(genotypes, missing_code=-9):
+    """Impute missing genotypes (-9) with per-variant mean"""
+    m = genotypes == missing_code
+    if genotypes.ndim == 1:
+        if m.any():
+            genotypes[m] = genotypes[~m].mean()
     else:  # genotypes.ndim == 2
-        ix = np.nonzero(m)[0]
-        if len(ix) > 0:
-            a = genotypes.sum(1)
-            b = m.sum(1)
-            mu = (a + 9*b) / (genotypes.shape[1] - b)
-            genotypes[m] = mu[ix]
+        row_means = np.where(m.any(1), genotypes.clip(min=0).mean(1), 0)
+        genotypes[m] = np.take(row_means, np.nonzero(m)[0])
+    return genotypes
 
 
 class PgenReader(object):
@@ -312,95 +41,97 @@ class PgenReader(object):
     Class for reading genotype data from PLINK 2 pgen files
 
     To generate the pgen/psam/pvar files from a VCF, run
-        plink2 --vcf ${vcf_file} --output-chr chrM --out ${plink_prefix_path}
+        plink2 --vcf ${vcf_file} --output-chr chrM --out ${plink_prefix}
     To use dosages, run:
-        plink2 --vcf ${vcf_file} 'dosage=DS' --output-chr chrM --out ${plink_prefix_path}
+        plink2 --vcf ${vcf_file} 'dosage=DS' --output-chr chrM --out ${plink_prefix}
 
     Requires pgenlib: https://github.com/chrchang/plink-ng/tree/master/2.0/Python
     """
-    def __init__(self, plink_prefix_path, select_samples=None):
+    def __init__(self, plink_prefix, select_samples=None, impute=True,
+                 dtype=np.float32, device="cpu"):
         """
-        plink_prefix_path: prefix to PLINK pgen,psam,pvar files
+        plink_prefix: prefix to PLINK pgen,psam,pvar files
         select_samples: specify a subset of samples
         """
-
-        if os.path.exists(f"{plink_prefix_path}.pvar.parquet"):
-            self.pvar_df = pd.read_parquet(f"{plink_prefix_path}.pvar.parquet")
-        else:
-            self.pvar_df = read_pvar(f"{plink_prefix_path}.pvar")
-        self.psam_df = read_psam(f"{plink_prefix_path}.psam")
-        self.pgen_file = f"{plink_prefix_path}.pgen"
+        self.pvar_df = (
+            pd.read_parquet(f"{plink_prefix}.pvar.parquet")
+            if os.path.exists(f"{plink_prefix}.pvar.parquet")
+            else read_pvar(f"{plink_prefix}.pvar")
+        )
+        self.psam_df = read_psam(f"{plink_prefix}.psam")
+        self.pgen_file = f"{plink_prefix}.pgen"
 
         self.num_variants = self.pvar_df.shape[0]
         self.variant_ids = self.pvar_df['id'].tolist()
         self.variant_idx_dict = {i:k for k,i in enumerate(self.variant_ids)}
 
-        self.sample_id_list = self.psam_df.index.tolist()
-        self.set_samples(select_samples)
+        self.sample_ids = self.psam_df.index.tolist()
+        self.sample_idxs = None
+        if select_samples is not None:
+            self.set_samples(select_samples)
 
-        variant_df = self.pvar_df.set_index('id')[['chrom', 'pos']]
-        variant_df['index'] = np.arange(variant_df.shape[0])
-        self.variant_df = variant_df
-        self.variant_dfs = {c:g[['pos', 'index']] for c,g in variant_df.groupby('chrom', sort=False)}
+        self.variant_df = self.pvar_df.set_index('id')[['chrom', 'pos']].copy()
+        self.variant_df['index'] = np.arange(len(self.variant_df))
+        self.variant_dfs = {c:g[['pos', 'index']]
+                            for c,g in variant_df.groupby('chrom', sort=False)}
 
-    def set_samples(self, sample_ids=None, sort=True):
-        """
-        Set samples to load.
+        self.impute = impute
+        self.dtype = dtype
+        self.device = self._get_device(device)
 
-        Parameters
-        ----------
-        sample_ids : array_like
-            List of samples to select.
-        sort : bool
-            Preserve sample order from pgen file.
-        """
-        if sample_ids is None:
-            self.sample_ids = self.sample_id_list
-            self.sample_idxs = None
-        else:
-            sample_idxs = [self.sample_id_list.index(i) for i in sample_ids]
-            if sort:
-                sidx = np.argsort(sample_idxs)
-                sample_idxs = [sample_idxs[i] for i in sidx]
-                sample_ids = [sample_ids[i] for i in sidx]
-            self.sample_ids = sample_ids
-            self.sample_idxs = sample_idxs
+    def _get_device(self, device):
+        if device == "auto":
+            return "cuda" if torch.cuda.is_available() else "cpu"
+        return device
+    
+    def set_samples(self, sample_ids=None):
+        """Restrict to a subset of samples (order preserved)."""
+        sample_idxs = [self.sample_ids.index(i) for i in sample_ids]
+        self.sample_ids = sample_ids
+        self.sample_idxs = sample_idxs
 
-    def get_range(self, region, start=None, end=None):
+    def get_range(self, chrom, start=None, end=None):
         """
         Get variant indexes corresponding to region specified as 'chr:start-end', or as chr, start, end.
-
-        Parameters
-        ----------
-        region : str
-            Genomic region, defined as 'chr:start-end' (1-based, inclusive), or chromosome.
-        start : int
-            Start position of the genomic interval (if chromosome is provided in fist argument).
-        end : int
-            End position of the genomic interval (if chromosome is provided in fist argument).
-
-        Returns
-        -------
-        indexes : ndarray
-            [start, end] indexes (inclusive)
+        Return [start,end] indexes for variants in a region.
         """
-        if start is None and end is None:
-            if ':' in region:
-                chrom, pos = region.split(':')
-                start, end = [int(i) for i in pos.split('-')]
-            else:  # full chromosome selected
-                chrom = region
-                return self.variant_dfs[chrom]['index'].values[[0, -1]]
-        else:  # input is chr, start, end
-            chrom = region
+        vpos = self.variant_dfs[chrom]["pos"].values
+        lb = bisect.bisect_left(vpos, start) if start else 0
+        ub = bisect.bisect_right(vpos, end) if end else vpos.shape[0]
+        if lb < ub:
+            return self.variant_dfs[chrom]['index'].values[[lb, ub - 1]]
+        return []
 
-        lb = bisect.bisect_left(self.variant_dfs[chrom]['pos'].values, start)
-        ub = bisect.bisect_right(self.variant_dfs[chrom]['pos'].values, end)
-        if lb != ub:
-            r = self.variant_dfs[chrom]['index'].values[[lb, ub - 1]]
-        else:
-            r = []
-        return r
+    def read_list(self, variant_ids, dosages=False, to_torch=False):
+        """Read list of variants as numpy or torch."""
+        vix = [self.variant_idx[v] for v in variant_ids]
+        nvar, nsamp = len(vix), len(self.sample_ids)
+        arr = np.zeros((nvar, nsamp),
+                       dtype=np.float32 if dosages else np.int8)
+
+        with pg.PgenReader(self.pgen_file.encode(),
+                           sample_subset=self.sample_idxs) as r:
+            if dosages:
+                r.read_dosages_list(np.array(vix, dtype=np.uint32), arr)
+            else:
+                r.read_list(np.array(vix, dtype=np.uint32), arr)
+
+        if not dosages and self.impute:
+            arr = _impute_mean(arr)
+
+        arr = arr.astype(self.dtype, copy=False)
+
+        if to_torch or self.device == "cuda":
+            return torch.as_tensor(arr, device=self.device)
+        return arr
+
+    def read_range(self, start_idx, end_idx, impute_mean=True, dtype=np.float32):
+        """Read genotypes for range of variants as 0,1,2,-9; impute missing values (-9) to mean (default)."""
+        genotypes = read_range(self.pgen_file, start_idx, end_idx, sample_subset=self.sample_idxs,
+                               dtype=np.int8).astype(dtype)
+        if impute_mean:
+            _impute_mean(genotypes)
+        return pd.DataFrame(genotypes, index=self.variant_ids[start_idx:end_idx+1], columns=self.sample_ids)
 
     def read(self, variant_id, impute_mean=True, dtype=np.float32):
         """Read genotypes for an individual variant as 0,1,2,-9; impute missing values (-9) to mean (default)."""
@@ -410,23 +141,6 @@ class PgenReader(object):
         if impute_mean:
             _impute_mean(genotypes)
         return pd.Series(genotypes, index=self.sample_ids, name=variant_id)
-
-    def read_list(self, variant_ids, impute_mean=True, dtype=np.float32):
-        """Read genotypes for an list of variants as 0,1,2,-9; impute missing values (-9) to mean (default)."""
-        variant_idxs = [self.variant_idx_dict[i] for i in variant_ids]
-        genotypes = read_list(self.pgen_file, variant_idxs, sample_subset=self.sample_idxs,
-                              dtype=np.int8).astype(dtype)
-        if impute_mean:
-            _impute_mean(genotypes)
-        return pd.DataFrame(genotypes, index=variant_ids, columns=self.sample_ids)
-
-    def read_range(self, start_idx, end_idx, impute_mean=True, dtype=np.float32):
-        """Read genotypes for range of variants as 0,1,2,-9; impute missing values (-9) to mean (default)."""
-        genotypes = read_range(self.pgen_file, start_idx, end_idx, sample_subset=self.sample_idxs,
-                               dtype=np.int8).astype(dtype)
-        if impute_mean:
-            _impute_mean(genotypes)
-        return pd.DataFrame(genotypes, index=self.variant_ids[start_idx:end_idx+1], columns=self.sample_ids)
 
     def read_region(self, region, start_pos=None, end_pos=None, impute_mean=True, dtype=np.float32):
         """Read genotypes for variants in a genomic region as 0,1,2,-9; impute missing values (-9) to mean (default)."""
@@ -539,13 +253,13 @@ class PgenReader(object):
         return pd.DataFrame(np.corrcoef(g), index=variant_ids, columns=variant_ids)
 
 
-def load_dosages_df(plink_prefix_path, select_samples=None):
+def load_dosages_df(plink_prefix, select_samples=None):
     """
     Load dosages for all variants and all/selected samples as a dataframe.
 
     Parameters
     ----------
-    plink_prefix_path : str
+    plink_prefix : str
         Prefix to .pgen/.psam/.pvar files
     select_samples : array_like
         List of sample IDs to select. Default: all samples.
@@ -555,5 +269,5 @@ def load_dosages_df(plink_prefix_path, select_samples=None):
     dosages_df : pd.DataFrame (variants x samples)
         Genotype dosages for the selected samples.
     """
-    p = Pgen(plink_prefix_path, select_samples=select_samples)
+    p = Pgen(plink_prefix, select_samples=select_samples)
     return p.load_dosages_df()
