90c35277594c04d43d8c8ec07b452d59d0d08408
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-09-14 08:23:19 -0400

    trim genotypeio for clarity; will bring things back on future updates

--- src/localqtl/genotypeio.py
+++ src/localqtl/genotypeio.py
@@ -1,13 +1,7 @@
-import pandas as pd
-import tempfile
 import numpy as np
-import subprocess
-import os
-import gzip
-import sys
-import threading
-import queue
-import bisect
+import pandas as pd
+import os, gzip, sys
+import threading, queue, bisect
 from pandas_plink import read_plink
 
 sys.path.insert(1, os.path.dirname(__file__))
@@ -19,26 +13,7 @@ except ImportError as e:
     pgen = None
 
 
-gt_to_dosage_dict = {'0/0':0, '0/1':1, '1/1':2, './.':np.nan,
-                     '0|0':0, '0|1':1, '1|0':1, '1|1':2, '.|.':np.nan}
-
-
-def _check_dependency(name):
-    e = subprocess.call(f"which {name}", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    if e != 0:
-        raise RuntimeError(f"External dependency '{name}' not installed")
-
-
-def print_progress(k, n, entity):
-    s = f'\r    processing {entity} {k}/{n}'
-    if k == n:
-        s += '\n'
-    sys.stdout.write(s)
-    sys.stdout.flush()
-
-
 class BackgroundGenerator(threading.Thread):
-    # Adapted from https://github.com/justheuristic/prefetch_generator
     def __init__(self, generator, max_prefetch=10):
         threading.Thread.__init__(self)
         self.queue = queue.Queue(max_prefetch)
@@ -73,50 +48,20 @@ class BackgroundGenerator(threading.Thread):
 class background:
     def __init__(self, max_prefetch=10):
         self.max_prefetch = max_prefetch
-    def __call__(self,gen):
-        def bg_generator(*args,**kwargs):
-            return BackgroundGenerator(gen(*args,**kwargs), max_prefetch=self.max_prefetch)
+    def __call__(self, gen):
+        def bg_generator(*args, **kwargs):
+            return BackgroundGenerator(gen(*args, **kwargs), max_prefetch=self.max_prefetch)
         return bg_generator
 
 
-#------------------------------------------------------------------------------
-#  Functions for writing VCFs
-#------------------------------------------------------------------------------
-def _get_vcf_opener(vcfpath):
-    if vcfpath.endswith('.vcf.gz'):
-        return gzip.open(vcfpath, 'rt')
-    else:
-        return open(vcfpath)
+def print_progress(k, n, entity):
+    s = f'\r    processing {entity} {k}/{n}'
+    if k == n:
+        s += '\n'
+    sys.stdout.write(s)
+    sys.stdout.flush()
 
 
-def get_sample_ids(vcfpath):
-    """Get sample IDs from VCF"""
-    with _get_vcf_opener(vcfpath) as vcf:
-        for header in vcf:
-            if header[:2] == '##': continue
-            break
-    return header.strip().split('\t')[9:]
-
-
-def parse_genotypes(x, field='GT'):
-    """Convert list of genotypes (str) to np.float32"""
-    if field == 'GT':
-        g = np.float32([gt_to_dosage_dict[i] for i in x])
-    elif field == 'DS':
-        g = np.float32(x)
-    return g
-
-
-def _get_field_ix(line, field):
-    """Get position of field ('GT' or 'DS') in FORMAT"""
-    fmt = line[8].split(':')
-    if field not in fmt:
-        raise ValueError(f'FORMAT field does not contain {field}')
-    return fmt.index(field)
-
-#------------------------------------------------------------------------------
-#  Functions for loading regions/variants from VCFs
-#------------------------------------------------------------------------------
 def _impute_mean(g, missing=-9, verbose=False):
     """Impute rows to mean (in place)"""
     if not g.dtype in [np.float32, np.float64]:
@@ -268,115 +213,6 @@ def load_genotypes(genotype_path, select_samples=None, dosages=False):
     return genotype_df, variant_df
 
 
-def get_vcf_region(region_str, vcfpath, field='GT', sample_ids=None, select_samples=None, impute_missing=True):
-    """Load VCF region (str: 'chr:start-end') as DataFrame (requires tabix)"""
-    s = subprocess.check_output(f'tabix {vcfpath} {region_str}', shell=True)
-    s = s.decode().strip().split('\n')
-    s = [i.split('\t') for i in s]
-
-    if sample_ids is None:
-        sample_ids = get_sample_ids(vcfpath)
-    variant_ids = [i[2] for i in s]
-    pos_s = pd.Series([int(i[1]) for i in s], index=variant_ids)
-
-    ix = _get_field_ix(s[0], field)
-    g = np.array([parse_genotypes([i.split(':')[ix] for i in line[9:]], field=field) for line in s])
-    df = pd.DataFrame(g, index=variant_ids, columns=sample_ids)
-
-    if select_samples is not None:
-        df = df[select_samples]
-
-    if impute_missing:
-        n = 0
-        for v in df.values:
-            m = np.isnan(v)
-            if np.any(m):
-                v[m] = np.mean(v[~m])
-                n += 1
-        if n > 0:
-            print(f'    imputed at least 1 sample in {n} sites')
-
-    return df, pos_s
-
-
-def get_vcf_variants(variant_ids, vcfpath, field='GT', sample_ids=None):
-    """Load a set of variants in VCF as DataFrame (requires tabix)"""
-    variant_id_set = set(variant_ids)
-    with tempfile.NamedTemporaryFile() as regions_file:
-        df = pd.DataFrame([i.split('_')[:2] for i in variant_id_set], columns=['chr', 'pos'])
-        df['pos'] = df['pos'].astype(int)
-        df = df.sort_values(['chr', 'pos'])
-        df.to_csv(regions_file.name, sep='\t', index=False, header=False)
-        s = subprocess.check_output(f'tabix {vcfpath} --regions {regions_file.name}', shell=True)
-    s = s.decode().strip().split('\n')
-    s = [i.split('\t') for i in s]
-
-    if sample_ids is None:
-        sample_ids = get_sample_ids(vcfpath)
-
-    ix = _get_field_ix(s[0], field)
-    g = np.array([parse_genotypes([i.split(':')[ix] for i in line[9:]], field=field) for line in s])
-    g = np.array([i for i in g if -1 not in i])  # filter missing here instead of ValueError?
-
-    returned_variant_ids = [i[2] for i in s]
-    ix = [k for k,i in enumerate(returned_variant_ids) if i in variant_id_set]
-    g = np.array([g[i] for i in ix])
-    returned_variant_ids = [returned_variant_ids[i] for i in ix]
-    return pd.DataFrame(g.astype(np.float32), index=returned_variant_ids, columns=sample_ids)
-
-#------------------------------------------------------------------------------
-#  Generator classes for batch processing of genotypes/phenotypes
-#------------------------------------------------------------------------------
-class GenotypeGeneratorTrans(object):
-    def __init__(self, genotype_df, batch_size=50000, chr_s=None):
-        """
-        Generator for iterating over all variants (trans-scan)
-
-        Inputs:
-          genotype_df: Dataframe with genotypes (variants x samples)
-          batch_size: Batch size for GPU processing
-
-        Generates: genotype array (2D), variant ID array
-        """
-        self.genotype_df = genotype_df
-        self.batch_size = batch_size
-        self.num_batches = int(np.ceil(self.genotype_df.shape[0] / batch_size))
-        self.batch_indexes = [[i*batch_size, (i+1)*batch_size] for i in range(self.num_batches)]
-        self.batch_indexes[-1][1] = self.genotype_df.shape[0]
-        if chr_s is not None:
-            chroms, chr_ix = np.unique(chr_s, return_index=True)
-            s = np.argsort(chr_ix)
-            self.chroms = chroms[s]
-            chr_ix = list(chr_ix[s]) + [chr_s.shape[0]]
-            size_s = pd.Series(np.diff(chr_ix), index=self.chroms)
-            self.chr_batch_indexes = {}
-            for k,c in enumerate(self.chroms):
-                num_batches = int(np.ceil(size_s[c] / batch_size))
-                batch_indexes = [[chr_ix[k]+i*batch_size, chr_ix[k]+(i+1)*batch_size] for i in range(num_batches)]
-                batch_indexes[-1][1] = chr_ix[k+1]
-                self.chr_batch_indexes[c] = batch_indexes
-
-    def __len__(self):
-        return self.num_batches
-
-    @background(max_prefetch=6)
-    def generate_data(self, chrom=None, verbose=False, enum_start=1):
-        """Generate batches from genotype data"""
-        if chrom is None:
-            batch_indexes = self.batch_indexes
-            num_batches = self.num_batches
-        else:
-            batch_indexes = self.chr_batch_indexes[chrom]
-            num_batches = np.sum([len(i) for i in self.chr_batch_indexes.values()])
-
-        for k,i in enumerate(batch_indexes, enum_start):  # loop through batches
-            if verbose:
-                print_progress(k, num_batches, 'batch')
-            g = self.genotype_df.values[i[0]:i[1]]
-            ix = self.genotype_df.index[i[0]:i[1]]  # variant IDs
-            yield g, ix
-
-
 def get_cis_ranges(phenotype_pos_df, chr_variant_dfs, window, verbose=True):
     """
 
@@ -521,58 +357,3 @@ class InputGeneratorCis(object):
                 p = self.phenotype_df.values[[index_dict[i] for i in group_phenotype_ids]]
                 r = self.cis_ranges[g.index[0]]
                 yield p, self.genotype_df.values[r[0]:r[-1]+1], np.arange(r[0],r[-1]+1), group_phenotype_ids, group_id
-
-
-def get_chunk_size(memory_gb, samples):
-    """"""
-    return memory_gb * 1024**3 // samples
-
-
-def generate_paired_chunks(pgr, phenotype_df, phenotype_pos_df, chunk_size, window=1000000,
-                           dosages=False, verbose=True):
-    """
-    Generate paired genotype-phenotype chunks for large datasets where only a subset of
-    genotypes can be loaded into memory.
-
-    pgr: pgen.PgenReader
-    phenotype_df:     phenotype DataFrame (phenotypes x samples)
-    phenotype_pos_df: DataFrame defining position of each phenotype, with columns ['chr', 'pos'] or ['chr', 'start', 'end']
-    chunk_size: maximum number of variants to load into CPU memory
-    window: cis-window
-    dosages: load dosages (DS) from genotype files (default: GT)
-    """
-    variant_df = pgr.pvar_df.set_index('id')[['chrom', 'pos']]
-    cis_ranges, _ = get_cis_ranges(phenotype_pos_df, pgr.variant_dfs, window)
-    range_df = pd.DataFrame(cis_ranges, index=['start', 'end']).T
-    range_df = range_df.join(phenotype_pos_df['chr'])
-
-    if chunk_size == 'chr':
-        chrlen_s = range_df['chr'].value_counts(sort=False)
-        start_ixs = [0] + chrlen_s.cumsum().tolist()
-    else:
-        chunk_size = int(chunk_size)
-        # check chunk size
-        max_cis_var = (range_df['end'] - range_df['start'] + 1).max()
-        if not max_cis_var <= chunk_size:
-            raise ValueError(f"Max. chunk size must be at least largest cis-window ({max_cis_var})")
-
-        start_ixs = [0]
-        while start_ixs[-1] < range_df.shape[0]:
-            end_ix = bisect.bisect_left(range_df['end'].values, range_df['start'].values[start_ixs[-1]] + chunk_size)
-            start_ixs.append(end_ix)
-        start_ixs[-1] = range_df.shape[0]
-
-    nchunks = len(start_ixs) - 1
-    for ci in range(nchunks):
-        if verbose:
-            print(f"Processing genotype-phenotype chunk {ci+1}/{nchunks}")
-        ix = slice(start_ixs[ci], start_ixs[ci+1])
-        chunk_df = range_df[ix]
-        if chunk_size == 'chr':
-            assert (chunk_df['chr'] == chrlen_s.index[ci]).all()
-        if dosages:
-            gt_df = pgr.read_dosages_range(chunk_df['start'].values[0], chunk_df['end'].values[-1], dtype=np.float32)
-        else:
-            gt_df = pgr.read_range(chunk_df['start'].values[0], chunk_df['end'].values[-1], impute_mean=False, dtype=np.int8)
-        var_df = variant_df.iloc[chunk_df['start'].values[0]:chunk_df['end'].values[-1]+1]
-        yield gt_df, var_df, phenotype_df[ix], phenotype_pos_df[ix], ci
