0d8cd0e273a28c4b54a18802d4d7db7840ad5c7c
Kynon JM Benjamin <KrotosBenjamin@users.noreply.github.com>
2025-10-27 18:44:29 -0500

    Update __init__.py
    Fix type with extra 'import'

--- src/localqtl/__init__.py
+++ src/localqtl/__init__.py
@@ -8,7 +8,7 @@ from .phenotypeio import read_phenotype_bed
 from .utils import gpu_available
 from .genotypeio import PlinkReader, InputGeneratorCis
 from .haplotypeio import RFMixReader, InputGeneratorCisWithHaps
-from .pgen import import PgenReader
+from .pgen import PgenReader
 
 __all__ = [
     "read_phenotype_bed",
