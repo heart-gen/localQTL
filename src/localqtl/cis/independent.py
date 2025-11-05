0acc7b856bcd34b5d01036aab2a02bc20d1f4112
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-11-05 10:35:52 -0500

    fixing API update

--- src/localqtl/cis/independent.py
+++ src/localqtl/cis/independent.py
@@ -33,8 +33,8 @@ __all__ = [
     "map_independent",
 ]
 
-torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True
+torch.backends.cuda.matmul.fp32_precision = 'tf32'
+torch.backends.cudnn.conv.fp32_precision = 'tf32'
 
 
 def _auto_perm_chunk(n_variants: int, nperm: int, safety: float = 0.65) -> int:
